675f001c72c1749806b5f76367f6b3e44eca6a8f
Tory Doby <torydoby43@gmail.com>
2022-07-26 10:54:14 -0400

    update

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,21 +1,39 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
+     *
      * @param input
      * @return
      */
-    public Integer countDuplicates(String[] input){
-        return null;
+    public Map<String, Integer> countDuplicates(String[] input) {
+        //
+        Map<String, Integer> countMap = new HashMap<String, Integer>();
+        String[] nums = input.split(" ");
+        for (String num : nums) {
+            Integer integer = countMap.get(num);
+            if (integer == null) {
+                Integer put = countMap.put(num, 1);
+                    else{
+                    countMap.put(num, integer + 1);
+                }
+            }
+        }
+        return countMap;
     }
 
-    /**
-     * Example of overloading a function that accepts a Array of Integers
-     * @param input
-     * @return
-     */
-    public Integer countDuplicates(Integer[] input){
+   // public Integer countDuplicates(Integer[] input) {
+        /**
+         * Example of overloading a function that accepts a Array of Integers
+         * @param input
+         * @return
+         */
+
         return null;
+
     }
 }
